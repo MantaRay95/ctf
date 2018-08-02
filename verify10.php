3c3f9d410ec71b74593aa0c1a0445e7cd482a834
Viraj Malaka <mahanama.vm@gmail.com>
2018-08-02 02:54:10 -0400

    level 10 initial commit

---
+++ verify10.php
@@ -0,0 +1,21 @@
+<html>
+<body>
+<?php
+
+$str = $_POST["inputPassword2"];
+
+if (sha1($str) == "test")
+{
+    header('Location: http://localhost/ctf/level3.html');
+    exit;
+}
+else
+{
+    header('Location: http://localhost/ctf/level2.html');
+    exit;
+}
+
+?>
+
+</body>
+</html>
