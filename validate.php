b049f89b76b3a2e12530d9985a64998c42ddda95
Pavithra <pavithrarasanjana61@gmail.com>
2018-10-29 20:22:28 +0530

    commit

--- validate.php
+++ validate.php
@@ -6,11 +6,8 @@
 $lvlstatus="flag_captured";
 $val_string=$_POST["flag_unvalidated"].$_POST["level_code"].$lvlstatus;
 $flagid=(int)$_POST["level_id"];
-<<<<<<< HEAD
-$flags=array("error","cc6b946e7b6cf972e7337b6c6b45a78234a02e91","90f0ea693a75d0c672d5db020ad2c7515bde3cbb",sha1("flag3"),sha1("flag4"),sha1("flag5"),sha1("flag6"),sha1("flag7"),"99e88b7f0f95ab176bdee429757d1a315f881cbd","c257d74726d1c83cbc3084ea1b560d6c89764d06",sha1("flag10"),"32df8f7e1df3906f3463171d28ead95862d39d60",sha1("flag12"),sha1("flag13"),sha1("flag14"),sha1("flag15"));
-=======
-$flags=array("error",sha1("flag1"),sha1("flag2"),sha1("flag3"),sha1("flag3"),sha1("flag3"),sha1("flag3"),sha1("flag3"),sha1("flag3"),sha1("flag3"),"16b05537dcd7cecbbf37a3773cd1365e01178611",sha1("flag3"),sha1("flag3"),sha1("flag3"),sha1("flag3"),sha1("flag3"));
->>>>>>> 82c2684c5f33102070dadf2b3c04c31d60e13374
+$flags=array("error","cc6b946e7b6cf972e7337b6c6b45a78234a02e91","90f0ea693a75d0c672d5db020ad2c7515bde3cbb",sha1("flag3"),sha1("flag4"),sha1("flag5"),sha1("flag6"),sha1("flag7"),"99e88b7f0f95ab176bdee429757d1a315f881cbd","c257d74726d1c83cbc3084ea1b560d6c89764d06","16b05537dcd7cecbbf37a3773cd1365e01178611","32df8f7e1df3906f3463171d28ead95862d39d60",sha1("flag12"),sha1("flag13"),sha1("flag14"),sha1("flag15"));
+
 
 //echo sha1($_POST["level_code"])
 //echo $flagid;
